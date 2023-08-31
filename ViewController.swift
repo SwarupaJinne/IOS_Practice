9eac831f71f4bc6ed814fd8e8ed0db33ff7d6ae3
Swarupa_Jinne <122853189+SwarupaJinne@users.noreply.github.com>
2023-08-31 13:50:00 -0500

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  GreaterNumber
+//  VowelTester
 //
 //  Created by Jinne,Swarupa on 8/31/23.
 //
@@ -9,9 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var inputOL1: UITextField!
-    
-    @IBOutlet weak var inputOL2: UITextField!
+    @IBOutlet weak var inputOL: UITextField!
     
     @IBOutlet weak var outputOL: UILabel!
     
@@ -20,30 +18,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func greatestNumber(_ sender: UIButton) {
-        
-        //read the input and assign it to the variable
-        //convert the string to interger
-        //check if the first value greater than second value
-        //print the greatest
-        var input1 = Int(inputOL1.text!)
-        var input2 = Int(inputOL2.text!)
-        //int input1 = Integer.parseInt(NUM1)
-        //int input1 = Integer.parseInt(NUM2)
-        
     
-        
-        if(input1! > input2!){
-            outputOL.text = ("\(input1!) is the greatest number")
-        }
-        else if(input1! < input2!
-        ){
-            outputOL.text = ("\(input2!), is the greatest number")
-            
+    @IBAction func btnClicked(_ sender: UIButton) {
+        //Read the input text and assign it for variable
+        var input:String = inputOL.text!
+    
+        //check if input contains vowels
+        //if text have vowels(a,e,i,o,u) then print "original text has vowels
+        //else the original text has no vowels
+        if(input.contains("a")||input.contains("e")||input.contains("i")||input.contains("o")||input.contains("u")){
+            outputOL.text=("\(input), has vowels")
         }
         else{
-            outputOL.text = ("Both the numbers are equal")
+            outputOL.text=("\(input), has no vowels")
         }
+        
+        
         
     }
     
