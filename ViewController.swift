8b0513b14139a16e9cb6efdeaabd5eedd1191ebb
Swarupa_Jinne <122853189+SwarupaJinne@users.noreply.github.com>
2023-09-26 13:20:23 -0500

    calculate price

--- ViewController.swift
+++ ViewController.swift
@@ -1,17 +1,21 @@
 //
 //  ViewController.swift
-//  VoteChecker
+//  CalculatePrice
 //
-//  Created by Jinne,Swarupa on 9/5/23.
+//  Created by Jinne,Swarupa on 9/21/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var inputOL: UITextField!
     
-    @IBOutlet weak var outputOL: UILabel!
+    
+    @IBOutlet weak var AmountOutlet: UITextField!
+    
+    @IBOutlet weak var DiscountRateOutlet: UITextField!
+    
+    
+    @IBOutlet weak var DisplayLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,23 +23,18 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func CheckForEligibility(_ sender: UIButton) {
-        //read the input and assign it to a variable
-        //convert the input of string to int type
-        //check whether the age is > 18 if >18 print "The person is eligible for voting"
-        //else print "The person is not eligible for voting"
-        var input = inputOL.text
-        var output = outputOL.text
-        var number = Double(inputOL.text!) ?? 0
-        if (number >= 18) {
-            outputOL.text = "The person is eligible for voting"
-        }
-        else {
-            outputOL.text = "The person is not eligible for voting"
-            
-        }
+    @IBAction func PriceCalculator(_ sender: UIButton) {
+        //reading the input from amount, discountrate and assigning it to the variables
+        //converting the input to double value
+        //calculating the price after the discount
+        var inputAmount = Double(AmountOutlet.text!) ?? 0
+        var inputDiscountRate = Double(DiscountRateOutlet.text!) ?? 0
+        var Discount = (inputDiscountRate / inputAmount) * 100
+        var result = inputAmount - Discount
+        
+        DisplayLabel.text! = "The discount is \(result)"
+        
     }
     
-
 }
 
